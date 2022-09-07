e8b3ebb153c83553214624ae6d8ed8284a4fa13c
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2022-09-07 22:43:26 +0530

    smiley

--- assets/js/panda-script.js
+++ assets/js/panda-script.js
@@ -15,22 +15,22 @@ function fillArc(color, x, y, r, startAngle, endAngle) {
     ctx.fill();
 };
 
-const pi = Math.PI;
+const PI = Math.PI;
 
-strokeArc(10, '#000', 500, 250, 200, 0, 2*pi)
+strokeArc(10, '#000', 500, 250, 200, 0, 2*PI)
 
-fillArc('#000', 400, 200, 50, 0, 2*pi)
-fillArc('#fff', 400, 200, 30, 0, 2*pi);
-fillArc('#000', 403, 200, 20, 0, 2*pi);
+fillArc('#000', 400, 200, 50, 0, 2*PI)
+fillArc('#fff', 400, 200, 30, 0, 2*PI);
+fillArc('#000', 403, 200, 20, 0, 2*PI);
 
-fillArc('#000', 600, 200, 50, 0, 2*pi)
-fillArc('#fff', 600, 200, 30, 0, 2*pi);
-fillArc('#000', 597, 200, 20, 0, 2*pi);
+fillArc('#000', 600, 200, 50, 0, 2*PI)
+fillArc('#fff', 600, 200, 30, 0, 2*PI);
+fillArc('#000', 597, 200, 20, 0, 2*PI);
 
-fillArc('#000' , 500, 250, 30, 0, 2*pi);
+fillArc('#000' , 500, 250, 30, 0, 2*PI);
 
-strokeArc(10, '#000', 500, 250, 120, 0.2*pi, pi- 0.2*pi);
+strokeArc(10, '#000', 500, 250, 120, 0.2*PI, PI- 0.2*PI);
 
-fillArc('#000' , 640, 110, 45, 1.25*pi, .25*pi);
-fillArc('#000' , 360, 110, 45, .75*pi, 1.75*pi);
+fillArc('#000' , 640, 110, 45, 1.25*PI, .25*PI);
+fillArc('#000' , 360, 110, 45, .75*PI, 1.75*PI);
 
