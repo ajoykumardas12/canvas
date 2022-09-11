9dd874f8e3ac6b1729c4a7b070bb34dbdda734d1
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2022-09-11 19:25:46 +0530

    particles-system-init-and-bounce-update

---
+++ assets/js/shrinking-particles-script.js
@@ -0,0 +1,78 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
+
+//resize canvas when window is resized
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+//mouse position
+const mouse = {
+    x: undefined,
+    y: undefined,
+};
+//change mouse position on mousemove
+canvas.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+//define particle class
+class Particle {
+    constructor(){
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+
+        this.size = Math.random() * 15 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.size > 0.2){
+            this.size -= 0.1;
+        };
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+//initialize particle creation
+function init(){
+    for(let i = 0; i<100; i++){
+        particlesArray.push(new Particle());
+    }
+}
+init();
+
+//handle particle properties
+function handleParticles(){
+    for(let i = 0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+
+        if(particlesArray[i].size <= 0.3){
+            particlesArray.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+//animate
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
+    requestAnimationFrame(animate);
+};
+animate();
